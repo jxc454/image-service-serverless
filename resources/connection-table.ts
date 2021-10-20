3faa716f2719befbe94ad1345fbe6b9d146f4533
Jameson Cotter-Ciambotti <jameson.cotter@gmail.com>
2021-10-20 19:07:42 -0400

    fixing dynamo resource files

--- resources/connection-table.ts
+++ resources/connection-table.ts
@@ -1,38 +1,36 @@
 export default {
-  Resources: {
-    imageMeta: {
-      Type: 'AWS::DynamoDB::Table',
-      Properties: {
-        TableName: '${self:custom.stage}-connections',
-        AttributeDefinitions: [
-          {
-            AttributeName: 'location',
-            AttributeType: 'S',
+  connections: {
+    Type: 'AWS::DynamoDB::Table',
+    Properties: {
+      TableName: '${self:custom.stage}-connections',
+      AttributeDefinitions: [
+        {
+          AttributeName: 'location',
+          AttributeType: 'S',
+        },
+        {
+          AttributeName: 'connectionId',
+          AttributeType: 'S',
+        },
+        {
+          AttributeName: 'created',
+          AttributeType: 'N',
+        },
+      ],
+      KeySchema: [{ AttributeName: 'connectionId', KeyType: 'HASH' }],
+      GlobalSecondaryIndexes: [
+        {
+          IndexName: 'location-connectionId',
+          KeySchema: [
+            { AttributeName: 'location', KeyType: 'HASH' },
+            { AttributeName: 'connectionId', KeyType: 'RANGE' },
+          ],
+          Projection: {
+            ProjectionType: 'ALL',
           },
-          {
-            AttributeName: 'connectionId',
-            AttributeType: 'S',
-          },
-          {
-            AttributeName: 'created',
-            AttributeType: 'N',
-          },
-        ],
-        KeySchema: [{ AttributeName: 'connectionId', KeyType: 'HASH' }],
-        GlobalSecondaryIndexes: [
-          {
-            IndexName: 'location-connectionId',
-            KeySchema: [
-              { AttributeName: 'location', KeyType: 'HASH' },
-              { AttributeName: 'connectionId', KeyType: 'RANGE' },
-            ],
-            Projection: {
-              ProjectionType: 'ALL',
-            },
-          },
-        ],
-        BillingMode: 'PAY_PER_REQUEST',
-      },
+        },
+      ],
+      BillingMode: 'PAY_PER_REQUEST',
     },
   },
 }
